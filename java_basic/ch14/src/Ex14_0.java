19db249aa0a95fe5a792eea08b5f2c1d5488b566
mgkim-developer <mgkim.developer@gmail.com>
2022-07-30 23:13:56 +0900

    2022.07.30_(Ex14_0, Ex14_1, 함수형 인터페이스, ch14-5~6 README Update)

---
+++ java_basic/ch14/src/Ex14_0.java
@@ -0,0 +1,23 @@
+public class Ex14_0 {
+    public static void main(String[] args) {
+//        Object obj = (a, b) -> a > b ? a : b; // 람다식. 익명객체
+
+//        MyFunction2 f = new MyFunction2() {
+//            public int max(int a, int b) {  // 오버라이딩 규칙 - 접근 제어자는 좁게 못바꾼다.
+//                    return a > b? a : b ;
+//                }
+//            };
+
+        // 람다식을 다루기 위한 참조변수의 타입은 함수형 인터페이스로 한다.
+        MyFunction2 f = (a, b) -> a > b ? a : b;    // 람다식 익명객체
+
+        int value = f.max(3, 5);  // 함수형 인터페이스
+        System.out.println("value=" + value);
+    }
+}
+
+@FunctionalInterface    // 함수형 인터페이스는 단 하나의 추상 메서드만 가져야 함.
+interface MyFunction2 {
+//    public abstract int max(int a, int b);
+    int max(int a, int b);
+}
