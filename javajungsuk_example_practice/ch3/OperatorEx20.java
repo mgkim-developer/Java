b8f8daa1bbc233e6960c8dac0cc68e5df47612e7
mgkim-developer <mgkim.developer@gmail.com>
2023-01-03 15:09:58 +0900

    2023.01.03_(javajungsuk review ex3-20)

---
+++ javajungsuk_example_practice/ch3/OperatorEx20.java
@@ -0,0 +1,7 @@
+public class OperatorEx20 {
+    public static void main(String[] args) {
+        System.out.println(-10 % 8);
+        System.out.println(10 % -8);    // 나머지 연산자(%)는 나누는 수로 음수도 허용한다. 그러나 부호는 무시되므로 결과는 음쉬 절대값으로 나눈 나머지와 결과가 같다.
+        System.out.println(-10 % -8);
+    }
+}
